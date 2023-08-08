1af419a8319403736e4233bc8fa3d6559ef4aeda
aninha306 <ana-pontes@aluno.senai.br>
2023-08-08 14:31:03 -0300

    feat: create store post function

---
+++ assets/js/style.posts.js
@@ -0,0 +1,41 @@
+const posts = [];
+
+function savePost(){
+    const title = document.getElementById("title").value;
+    const category = document.getElementById("category").value;
+    const resume = document.getElementById("resume").value;
+    const author = document.getElementById("author").value;
+    const date = document.getElementById("date").value;
+
+    if(title && category && resume && author && date){
+        storePost (title, category, resume, author, date);
+    } else{
+        alert("Preencha todos os campos!")
+    }
+
+   storePost(title, category, resume, author, date);
+
+    cleanFields();
+}
+
+function cleanFields(){
+    document.getElementById("title").value = "";
+    document.getElementById("category").value = "";
+    document.getElementById("resume").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("date").value = "";
+}
+
+function storePost(title, category, resume, author, date) {
+    const post = {
+        title,
+        category,
+        resume,
+        author,
+        date
+    };
+
+    posts.push(post);
+
+    console.log(post)
+}
