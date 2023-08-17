764e40cf9bc40462486a49a6c7163719f7eef2f0
aninha306 <ana-pontes@aluno.senai.br>
2023-08-17 14:27:50 -0300

    feat: initiate html

--- assets/js/style.categories-products.js
+++ assets/js/style.categories-products.js
@@ -7,6 +7,7 @@ class Category {
     }
 }
 
+//Produto pertence a uma categoria = 1:1
 class Products {
     constructor(id, name, price, category) {
         this.id = id;
@@ -44,6 +45,7 @@ class ProductService {
 }
 
 const categoryList = new CategoryService();
+const productList = new ProductService();
 
 function createCategory() {
     const categoryName = "Doce";
@@ -51,4 +53,14 @@ function createCategory() {
     categoryList.addCategory(categoryName);
 
     console.log(categoryList.categories);
+}
+
+function createProduct(){
+    const productName = "Bolo";
+    const productPrice = 20;
+    const productCategory = categoryList.categories[0];
+
+    productList.addProduct(productName, productPrice, productCategory);
+
+    console.log(productList.products);
 }
