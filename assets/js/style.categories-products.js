05cb0cfa4c44246dfb3899970480267119dedb7a
aninha306 <ana-pontes@aluno.senai.br>
2023-08-17 10:17:55 -0300

    feat: add class Product

---
+++ assets/js/style.categories-products.js
@@ -0,0 +1,17 @@
+//Categoria possui vários produtos = 1:N
+class Category {
+    constructor(id, name){
+        this.id = id;
+        this.name = name;
+        this.products = [];
+    }
+}
+
+class Products {
+    constructor(id, name, price, category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
